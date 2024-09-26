982aff4aed477f89fc89ed25ab85896430abca4f
simar <simar.apt22@gmail.com>
2024-09-26 13:58:17 +0530

    abh tk

---
+++ scripts.js
@@ -0,0 +1,62 @@
+// alert("hehe")
+
+// var arr=[1,2,3,true,{},function(){},[],"simar"]
+
+// we can store any item in js array jaroori nhi ki similar ho
+
+//forEach, map,filter,find,indexOf
+
+var arr=[1,2,2,3,4]
+// arr ke har ke liye function chalao
+// 1. forEach
+// arr.forEach(function(val){
+//     console.log(val + " hello");
+// })
+
+// 2. map
+// var newarr=arr.map(function(val){
+//      return val+13;
+// })
+
+// console.log(newarr);
+
+//3. filter
+// og array se new array ban na and vo array og se chota and same size ka hoga tab use krte filter
+// var ans= arr.filter(function(val){
+//     if(val>=2){
+//         return true;
+//     }
+//     else return false;
+// })
+// console.log(ans);
+
+//4.find 
+// yeh array vo element dhund ke dega
+// var ans=arr.find(function(val){
+//     if(val===3){
+//         return val;
+//     }
+// })
+// console.log(ans);
+
+//5.indexOf
+//index batata h, agr 2 honge toh 1st occurence ka index deta h
+var ans=arr.indexOf(2)
+console.log(ans)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ scripts2.js
@@ -0,0 +1,23 @@
+var obj={
+    name:"simar",
+    age: 25,
+    // kuchbhi:kuchbhi
+
+}
+
+// object hota h key-value pairs
+
+//agr hum chahte ke value obj vali show ho toh we use freeze
+Object.freeze(obj);
+
+obj.name
+obj.age=30
+obj['name']
+//just ways to access from object 
+
+var arr=[1,2,3,4]
+function abcd(a,b,c){
+
+}
+//no. of parameters is length function mai
+// because functions are objects in js
